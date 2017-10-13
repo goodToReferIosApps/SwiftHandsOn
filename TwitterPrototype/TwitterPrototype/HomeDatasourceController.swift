9f8dab2ad4c3000b47111d90ad6e742728f80c6f
Patnayak, Nilikh <Nilikh.Patnayak@searshc.com>
2017-10-13 13:22:40 -0500

    Checked in hands-on sample tried in swift so far.

---
+++ TwitterPrototype/TwitterPrototype/HomeDatasourceController.swift
@@ -0,0 +1,33 @@
+//
+//  HomeDatasourceController.swift
+//  TwitterPrototype
+//
+//  Created by Patnayak, Nilikh on 10/3/17.
+//  Copyright © 2017 Patnayak, Nilikh. All rights reserved.
+//
+
+import LBTAComponents
+
+class HomeDatasourceController: DatasourceController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+        let homeDatasource = HomeDatasource()
+        self.datasource = homeDatasource
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 80)
+    }
+
+}
